9a9c7b4c539b9804c4a37a6c4cbfee7cf6caacc5
Hiago 7k <hiago7k@gmail.com>
2026-04-29 18:48:41 -0300

    exibindo em tela os dados

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 ﻿
 
+using System.Text;
 
-Console.WriteLine("Testandos");
+var enderecoDoArquivo = "pessoas.txt";
+
+var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+var buffer = new byte[1024];
+fluxoDoArquivo.Read(buffer, 0, 1024);
+
+
+var utf8 = new UTF8Encoding();
+var texto = utf8.GetString(buffer);
+Console.WriteLine(texto);
+
+
