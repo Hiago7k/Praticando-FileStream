7a1fae412191b6aba59114ef394210fcb610f55a
Hiago 7k <hiago7k@gmail.com>
2026-04-30 18:43:58 -0300

    estruturando

--- Program.cs
+++ Program.cs
@@ -1,16 +1,4 @@
-﻿
-
-using System.Text;
-
-var enderecoDoArquivo = "pessoas.txt";
-
-var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-var buffer = new byte[1024];
-fluxoDoArquivo.Read(buffer, 0, 1024);
+﻿using System.Text;
 
 
-var utf8 = new UTF8Encoding();
-var texto = utf8.GetString(buffer);
-Console.WriteLine(texto);
-
-
+var caminhoDoArquivo = "pessoas.txt";
