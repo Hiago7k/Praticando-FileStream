4f9d8af7c27fdeade978d89fa9dfdedbe680b8bd
Hiago 7k <hiago7k@gmail.com>
2026-04-30 18:48:03 -0300

    lendo o arquivo

--- Program.cs
+++ Program.cs
@@ -2,3 +2,15 @@
 
 
 var caminhoDoArquivo = "pessoas.txt";
+
+
+var fluxoDoArquvio = new FileStream(caminhoDoArquivo, FileMode.Open);
+var buffer = new byte[1024];
+
+fluxoDoArquvio.Read(buffer, 0, 1024);
+
+var utf8 = new UTF8Encoding();
+var text = utf8.GetString(buffer);
+
+Console.WriteLine(text);
+
